4c60b74837b4b7ab018bac630e50e6b0f067a681
gerry <gerry@ajax.home.lan>
2016-08-12 15:29:19 +0100

    Added code comments

--- src/main/java/org/f8s/g5/util/Streams.java
+++ src/main/java/org/f8s/g5/util/Streams.java
@@ -3,6 +3,12 @@ package org.f8s.g5.util;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ * I exist because Java-8 Streams are slightly bonkers in their API.
+ * 
+ * @author gerard.fernandes@gmail.com
+ *
+ */
 public class Streams {
 	
 	public static <T> Stream<T> sequential(Iterable<T> iterable) {
