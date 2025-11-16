6a6fc1a75e925983a05885b1a746e6fca8f6f0bc
Gerard Fernandes <gerard.fernandes@gmail.com>
2025-11-16 19:27:32 +0000

    Added ability to iterate over words. Words can be indicated by patterns describing characters of interest, and delimiters. Only characters matching patterns of interest will be collected into coherent "words" - others will be rejected.

--- src/main/java/org/g5/util/InputStreamTokenIterator.java
+++ src/main/java/org/g5/util/InputStreamTokenIterator.java
@@ -2,11 +2,17 @@ package org.g5.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Iterator;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 /**
- * I an an {@link Iterator iterator} over an {@link java.io.InputStream input-stream}.
- * I provide the ability to iterate over tokenised data in an {@link java.io.InputStream input-stream}.
+ * I am an {@link Iterator iterator} over tokenised values read from a {@link Reader reader}.
+ * I provide the ability to iterate over tokenised data - words - in a {@link Reader reader}.<br/><p/>
+ * I support selector and delimiter patterns allowing flexibility in collecting values of interest into
+ * coherent collections of characters. These could be words, identifiers or any other collection of characters
+ * that make sense to the usage context.<br/><p/>
  * In conjunction with an appropriate {@link java.util.Spliterator spliterator}, I could be used to batch
  * these tokens and process these batches without waiting for the full stream to complete loading.
  * This can be powerful in terms of reducing latency for large data streams where there are a large
@@ -19,16 +25,22 @@ public class InputStreamTokenIterator implements Iterator<String> {
     private static final Character START_ARRAY = '[';
     private static final Character END_ARRAY = ']';
 
-    private final BufferedReader inputStreamReader;
-    private final Character token;
+    private final Reader inputStreamReader;
+    private final Predicate<Character> selector;
+    private final Predicate<Character> excluder;
     private Character nextChar;
-    private StringBuilder currentToken;
+    private StringBuilder currentValue;
 
-    public InputStreamTokenIterator(BufferedReader inputStreamReader, Character token) {
+    public InputStreamTokenIterator(Reader inputStreamReader, Pattern selectorPattern, Character delimiter) {
+        this(inputStreamReader, selectorPattern, Pattern.compile("(" + delimiter + "|\\" + END_ARRAY + "|\\" + START_ARRAY + ")"));
+    }
+
+    public InputStreamTokenIterator(Reader inputStreamReader, Pattern selectorPattern, Pattern delimiterPattern) {
         this.inputStreamReader = inputStreamReader;
-        this.token = token;
+        this.selector = characterSelector(selectorPattern);
+        this.excluder = delimiterPatternExcluder(delimiterPattern);
         this.nextChar = null;
-        this.currentToken = new StringBuilder();
+        this.currentValue = new StringBuilder();
     }
 
     @Override
@@ -46,11 +58,15 @@ public class InputStreamTokenIterator implements Iterator<String> {
         String nextToken = null;
         try {
             for (; nextChar != Character.MAX_VALUE; nextChar = (char) inputStreamReader.read()) {
-                if (canAccept(nextChar)) {
-                    this.currentToken.append(nextChar);
-                } else if (endOfTokenOrList()){
-                    nextToken = this.currentToken.toString();
-                    this.currentToken = new StringBuilder();
+                if (this.selector.test(nextChar)) {
+                    this.currentValue.append(nextChar);
+                } else if (this.excluder.test(nextChar)){
+                    nextToken = this.currentValue.toString();
+                    nextToken = nextToken.endsWith(".")? nextToken.substring(0, nextToken.length()-1) : nextToken;
+                    this.currentValue = new StringBuilder();
+                    if(nextToken.isEmpty()) {
+                        continue;
+                    }
                     return nextToken;
                 }
             }
@@ -60,12 +76,12 @@ public class InputStreamTokenIterator implements Iterator<String> {
         return nextToken;
     }
 
-    private boolean endOfTokenOrList() {
-        return nextChar == token || nextChar == END_ARRAY;
+    private Predicate<Character> delimiterPatternExcluder(final Pattern tokenPattern) {
+        return ch -> tokenPattern.matcher(Character.toString(ch)).matches();
     }
 
-    private boolean canAccept(Character character) {
-        return Character.isLetterOrDigit(character) && ! character.equals(token);
+    private Predicate<Character> characterSelector(final Pattern selectorPattern) {
+        return ch -> selectorPattern.matcher(Character.toString(ch)).matches();
     }
 
 }
--- src/test/java/org/g5/util/InputStreamTokenIteratorTest.java
+++ src/test/java/org/g5/util/InputStreamTokenIteratorTest.java
@@ -2,14 +2,15 @@ package org.g5.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Spliterator;
-import java.util.Spliterators;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
@@ -19,27 +20,162 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class InputStreamTokenIteratorTest {
 
+    private static final Logger log = LoggerFactory.getLogger(InputStreamTokenIteratorTest.class);
+    private static final int TEST_ID_LENGTH = 6;
+
     @Test
     public void shouldIterateOverTokensInInputStream() throws Exception {
         int numberOfDataItems = 10_000;
-        String testData = generateTestData(6, numberOfDataItems);
+        String testData = generateTestData(TEST_ID_LENGTH, numberOfDataItems);
         ByteArrayInputStream testInputStream = new ByteArrayInputStream(testData.getBytes());
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(testInputStream));
 
-        InputStreamTokenIterator inputStreamTokenIterator = new InputStreamTokenIterator(reader, ',');
+        InputStreamTokenIterator inputStreamTokenIterator = new InputStreamTokenIterator(reader, Pattern.compile("\\w"), ',');
 
         List<String> tokenList = StreamSupport.stream(Spliterators.spliteratorUnknownSize(inputStreamTokenIterator, Spliterator.ORDERED), false)
                 .toList();
 
-        System.out.println(tokenList);
+        log.info("Token-list: {}.", tokenList);
         assertThat(tokenList.size(), equalTo(numberOfDataItems));
         assertThat(tokenList, equalTo(Arrays.asList(testData.replaceAll("(\\[|\\])", "").split(","))));
     }
 
+    @Test
+    public void shouldIterateOverWords() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("/text-data.txt"))));
+        InputStreamTokenIterator inputStreamTokenIterator = new InputStreamTokenIterator(reader, Pattern.compile("(\\w|'|-|\\.)"), Pattern.compile("\\W"));
+
+        List<String> words = StreamSupport.stream(Spliterators.spliteratorUnknownSize(inputStreamTokenIterator, Spliterator.ORDERED), false)
+                .toList();
+        log.info("Token-list: {}.", words);
+        assertThat(words, equalTo(expectedWords()));
+
+        Map<String, Long> distinctWordCount = words.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), //group-by distinct word
+                        TreeMap::new,                               //...into a sorted Map
+                        Collectors.counting()));                    //...counting number of time word is seen
+        log.info("\n{}", distinctWordCount.entrySet().stream().map(e -> e.getKey() + ":" + e.getValue() + "\n").collect(Collectors.joining()));
+    }
+
+    @SuppressWarnings("all")
     private String generateTestData(int length, int count) {
         return "[" + IntStream.range(0, count).mapToObj(i -> RandomStringUtils.random(length, 50, 120, true, true))
                 .collect(Collectors.joining(",")) + "]";
     }
 
+    private List<String> expectedWords() {
+        return List.of("The",
+                "first",
+                "model",
+                "of",
+                "Aston",
+                "Martin's",
+                "second-century",
+                "plan",
+                "the",
+                "DB11",
+                "like",
+                "its",
+                "predecessor",
+                "and",
+                "its",
+                "platform",
+                "siblings",
+                "incorporates",
+                "aluminium",
+                "extensively",
+                "throughout",
+                "its",
+                "body",
+                "Official",
+                "manufacture",
+                "of",
+                "the",
+                "DB11",
+                "began",
+                "at",
+                "the",
+                "Aston",
+                "Martin",
+                "facility",
+                "in",
+                "Gaydon",
+                "Warwickshire",
+                "in",
+                "September",
+                "2016",
+                "Two",
+                "engine",
+                "configurations",
+                "of",
+                "the",
+                "DB11",
+                "were",
+                "available",
+                "a",
+                "4.0-litre",
+                "V8-engine",
+                "model",
+                "produced",
+                "by",
+                "Mercedes-AMG",
+                "and",
+                "a",
+                "5.2-litre",
+                "V12-engine",
+                "model",
+                "produced",
+                "by",
+                "Aston",
+                "Martin",
+                "The",
+                "Volante",
+                "version",
+                "of",
+                "the",
+                "DB11",
+                "was",
+                "introduced",
+                "in",
+                "October",
+                "2017",
+                "In",
+                "2018",
+                "Aston",
+                "Martin",
+                "and",
+                "its",
+                "racing",
+                "division",
+                "replaced",
+                "the",
+                "DB11",
+                "V12",
+                "with",
+                "the",
+                "DB11",
+                "V12",
+                "AMR",
+                "which",
+                "brought",
+                "an",
+                "increased",
+                "engine",
+                "output",
+                "The",
+                "V8-powered",
+                "model",
+                "also",
+                "received",
+                "an",
+                "enhancement",
+                "in",
+                "engine",
+                "performance",
+                "in",
+                "2021"
+        );
+    }
 }
